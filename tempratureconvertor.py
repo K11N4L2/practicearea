f0395a3b9731887325b8bc06b2ee295ed45419a4
K11N4L2 <59125204+K11N4L2@users.noreply.github.com>
2020-02-02 18:04:20 +0000

    Create tempratureconvertor.py

---
+++ tempratureconvertor.py
@@ -0,0 +1,20 @@
+print('Welcome to the temperature converter')
+
+#tempi = print('choose your temp; \nC: enter 1\nF: enter 2 \nK: enter 3 ')
+temp_f = float(input('\nWhat temperature in Fahrenheit would you like to convert ? '))
+
+# if statement for option for tempi
+# set measurement 
+# display conversion as table
+
+# c and k - create function add in input for different temps
+temp_c = (5/9)*(temp_f - 32)
+temp_k = temp_c + 273.15
+
+temp_f = round(temp_f, 4)
+temp_c = round(temp_c, 4)
+temp_k = round(temp_k, 4)
+
+print('\nDegrees Fahrenheit:\t' + str(temp_f))
+print('Degrees Celsius:\t' + str(temp_c))
+print('Degrees Kelvin:\t\t' + str(temp_k))
